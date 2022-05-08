269cee53d24522eae779788d8133a25530a05740
Jonas Galvez <jonasgalvez@gmail.com>
2022-05-08 13:28:27 -0300

    wip

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 {
-  if (typeof window !== 'undefined') {  
+  if (typeof window !== 'undefined') {
+    const kRoute = Symbol('kRoute')
     const kHead = Symbol('kHead')
     const kHeadElements = Symbol('kHeadElements')
 
@@ -18,21 +19,59 @@
     }
 
     for (const tag in window[kHead]) {
+      window[kHeadElements][tag] = Array.isArray(window[kHead][tag])
+        ? [...document.querySelectorAll(tag)]
+        : document.querySelector(tag)
       window[kHead][tag] = Array.isArray(window[kHead][tag])
         ? [...document.querySelectorAll(tag)].map(getElementData)
         : getElementData(document.querySelector(tag))
     }
 
     window.head = new Proxy({}, {
-      get (_, prop) {
-        return window[kHead][prop]
+      get (_, elem) {
+        return window[kHead][elem]
       },
-      set (_, prop, value) {
-        if (['title', 'base']) {
-
+      set (_, elem, value) {
+        switch (elem) {
+          case 'title':
+            setElement(elem, { value })
+            break
+          case 'base':
+            setElement(elem, {
+              attrs: value,
+            })
+            break
+          // case 'link':
+          //   setElements(elem, {
+          //     attrs: value,
+          //   })
+          //   break
+          default:
+            break
         }
       }
     })
+
+    function setElement (elem, data) {
+      if (window[kHeadElements][elem]) {
+        window[kHeadElements][elem].innerText = data.value
+        if (data.attrs) {
+          for (const [attr, value] of Object.entries(data.attrs)) {
+            window[kHeadElements][elem].setAttribute(attr, value)
+          }
+        }
+      } else {
+        const node = document.createElement(elem)
+        node.innerText = data.value
+        if (data.attrs) {
+          for (const [attr, value] of Object.entries(data.attrs)) {
+            node.setAttribute(attr, value)
+          }
+        }
+        window[kHeadElements][elem] = elem
+        document.head.appendChild(node)
+      }
+    }
 
     function getElementData (elem) {
       if (!elem) {
