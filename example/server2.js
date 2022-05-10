c28fd4868f9e901862e5341a77d3b287c48bd9f6
Jonas Galvez <jonasgalvez@gmail.com>
2022-05-10 16:59:24 -0300

    debugging perf

--- example/server2.js
+++ example/server2.js
@@ -3,33 +3,33 @@ import { createReadStream } from 'node:fs'
 import { dirname, resolve } from 'node:path'
 import { Readable } from 'node:stream'
 import { createSSRApp } from 'vue'
-import { renderToNodeStream } from '@vue/server-renderer'
+import { renderToString } from '@vue/server-renderer'
 import Head from 'unihead'
 
 const server = Fastify()
 
-async function * streamHtml (head, body) {
-  for (const chunk of head) {
-    yield chunk
-  }
-  for await (const chunk of body) {
-    yield chunk
-  }
-}
+// async function * streamHtml (head, body) {
+//   for (const chunk of head) {
+//     yield chunk
+//   }
+//   for await (const chunk of body) {
+//     yield chunk
+//   }
+// }
 
-server.decorateReply('html', function (head, body) {
-  this.send(Readable.from(streamHtml(head, body)))
-})
+// server.decorateReply('html', function (head, body) {
+//   this.send(Readable.from(streamHtml(head, body)))
+// })
 
 server.get('/', async (req, reply) => {
   reply.type('text/html')
-  reply.html(
+  reply.send(
     new Head({
       title: 'Page title',
       base: { href: '/', target: '_blank' },
       meta: [{ name: 'twitter:title', content: 'Title' }],
-    }),
-    renderToNodeStream(createSSRApp({
+    }).render() +
+    await renderToString(createSSRApp({
       data: () => ({
         message: 'Combined Streaming!',
         total: 1000,
